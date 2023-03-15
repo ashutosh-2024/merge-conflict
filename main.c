b076ef951225588c9f578a3debfcaffdfcb7569d
Ashutosh Aswani <202051215@iiitvadodara.ac.in>
2023-03-15 15:57:41 +0530

    1

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
-pseudo code
-{
-Initial Function()
-{
-  //Sample Code
+#include <stdio.h>
+
+int feature1() {
+  return 1;
 }
 
-feature1()
-{
-  //Sample Code for Feature 1
-}
-
-
-
+int main() {  
+  return 0;
 }
